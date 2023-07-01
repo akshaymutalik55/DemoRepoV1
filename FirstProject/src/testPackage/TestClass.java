32f11873ff784c46a9881d7bc03ab5966d4d531f
akshaymutalik55 <akshaymutalik93@gmail.com>
2023-07-01 15:47:00 +0530

    Basic Task Commit

---
+++ FirstProject/src/testPackage/TestClass.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class TestClass {
+	public static void main(String args[])
+	{
+		System.out.println("Test class");
+		System.out.println("Util class");
+		System.out.println("pageLayer class");
+		System.out.println("TestLayer class");
+	}
+
+}
